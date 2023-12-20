e034e5493a4c4dbd27c928d986c360eaa798d02c
ChoHyeonWoo <hwcho0456@gmail.com>
2023-12-20 16:36:00 +0900

    fix: change site config

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Next.js + NextUI",
-	description: "Make beautiful websites regardless of your design experience.",
+	name: "study korean with k-contents",
+	description: "study korean with k-contents",
 	navItems: [
 		{
 			label: "서비스 소개",
