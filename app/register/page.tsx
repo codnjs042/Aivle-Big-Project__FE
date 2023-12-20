3725c383e9d173cfb515a6aee4d71466c425dd5e
ChoHyeonWoo <hwcho0456@gmail.com>
2023-12-21 01:01:42 +0900

    feat: change register with multipage

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -5,30 +5,35 @@ import {
   Button,
   Checkbox,
   CheckboxGroup,
-  Divider,
   Input,
   Listbox,
   ListboxItem,
-  Spacer,
+  Spacer
 } from "@nextui-org/react";
 import {
-  LockFilledIcon,
-  MailIcon,
-  SunFilledIcon,
   AvatarIcon,
   EditIcon,
+  LockFilledIcon,
+  MailIcon,
   MonitorMobileIcon,
+  SunFilledIcon,
 } from "@nextui-org/shared-icons";
-import { Link } from "@nextui-org/link";
+import PrivacyPolicy from "@/components/modals/privacyPolicy";
+import TermOfUse from "@/components/modals/termOfUse";
 
 export default function SignupPage() {
+  const [step, setStep] = useState(0);
+  const [agreePrivacy, setAgreePrivacy] = useState(false);
+  const [agreeTerms, setAgreeTerms] = useState(false);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [number, setNumber] = useState("");
-  const [rememberMe, setRememberMe] = useState(false);
   const [selectedGenres, setSelectedGenres] = useState<string[]>([]);
   const [selectedArtist, setSelectedArtist] = useState(new Set(["start1"]));
+
+  const nextStep = () => setStep(step + 1);
+  const prevStep = () => setStep(step - 1);
 
   const handleSubmit = (e: { preventDefault: () => void }) => {
     e.preventDefault();
@@ -36,133 +41,230 @@ export default function SignupPage() {
       name,
       email,
       password,
-      rememberMe,
       selectedArtist,
       selectedGenres,
     });
   };
-  return (
-    <div className="flex flex-col gap-4 p-6 bg-top rounded-lg shadow-lg">
-      <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 justify-center items-center">
-        <p className="text-3xl center">회원가입</p>
-      </div>
-      <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
-        <Input
-          type="string"
-          label="아이디"
-          labelPlacement="outside"
-          startContent={
-            <EditIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-          }
-        />
-      </div>
-      <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
-        <Input
-          type="password"
-          label="비밀번호"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          labelPlacement="outside"
-          startContent={
-            <LockFilledIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-          }
-        />
-      </div>
-      <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
-        <Input
-          type="password"
-          label="비밀번호 재확인"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          labelPlacement="outside"
-          startContent={
-            <LockFilledIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-          }
-        />
-      </div>
-      <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
-        <Input
-          type="string"
-          label="이름"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          labelPlacement="outside"
-          startContent={
-            <AvatarIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-          }
-        />
-      </div>
-      <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
-        <Input
-          type="email"
-          label="이메일"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          labelPlacement="outside"
-          startContent={
-            <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-          }
-        />
-      </div>
-      <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
-        <Input
-          type="number"
-          label="휴대전화"
-          value={number}
-          onChange={(e) => setNumber(e.target.value)}
-          labelPlacement="outside"
-          startContent={
-            <MonitorMobileIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-          }
-        />
-      </div>
-      <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
-        <Divider className="my-4" />
-      </div>
-      <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
-        <CheckboxGroup
-          className="gap-5"
-          label="선호하는 장르"
-          orientation="horizontal"
-          value={selectedGenres}
-          onValueChange={setSelectedGenres}
-        >
-          <Checkbox value="action">액션</Checkbox>
-          <Checkbox value="drama">드라마</Checkbox>
-          <Checkbox value="comedy">코미디</Checkbox>
-          <Checkbox value="romance">로맨스</Checkbox>
-          <Checkbox value="history">사극</Checkbox>
-        </CheckboxGroup>
-      </div>
-      <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
-        <Listbox
-          className="gap-5"
-          aria-label="Favorite Artist"
-          label="Favorite Contents"
-          variant="flat"
-          disallowEmptySelection
-          selectionMode="multiple"
-          selectedKeys={selectedArtist}
-          onSelectionChange={(keys) => setSelectedArtist(keys as Set<string>)}
-        >
-          <ListboxItem key="start1">BTS</ListboxItem>
-          <ListboxItem key="start2">BLACKPINK</ListboxItem>
-          <ListboxItem key="start3">NEWJEANS</ListboxItem>
-        </Listbox>
-      </div>
-      <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
-        <Spacer y={1} />
-      </div>
-      <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 justify-center items-center">
-        <Button color="secondary" fullWidth={true} onClick={handleSubmit}>
-          회원가입
-        </Button>
-      </div>
-      <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 justify-center items-center">
-        <SunFilledIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-        <SunFilledIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-        <SunFilledIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-      </div>
-    </div>
-  );
+
+  switch (step) {
+    case 0:
+      return (
+          <div className="flex flex-col gap-4 p-6 bg-top rounded-lg shadow-lg">
+            <div
+                className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 justify-center items-center">
+              <p className="text-3xl center">회원가입</p>
+            </div>
+            <div
+                className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 justify-center items-center">
+              <p className="text-2xl center">Step 0</p>
+            </div>
+            <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 justify-between">
+              <PrivacyPolicy/>
+              <Checkbox color="secondary" isSelected={agreePrivacy} onValueChange={setAgreePrivacy}>
+                개인정보 처리방침에 동의합니다.
+              </Checkbox>
+            </div>
+            <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 justify-between">
+              <TermOfUse/>
+              <Checkbox color="secondary" isSelected={agreeTerms} onValueChange={setAgreeTerms}>
+                서비스 이용약관에 동의합니다.
+              </Checkbox>
+            </div>
+            <div
+                className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 justify-center items-center">
+              <Button color="secondary" variant = {(agreePrivacy && agreeTerms) ? "solid" : "flat"} fullWidth={true} onClick={nextStep} disabled={!(agreePrivacy && agreeTerms)}>
+                {(agreePrivacy && agreeTerms) ? "다음 단계로" : "모든 항목을 체크해주세요"}
+              </Button>
+
+            </div>
+          </div>
+      );
+    case 1:
+      return (
+          <div className="flex flex-col gap-4 p-6 bg-top rounded-lg shadow-lg">
+            <div
+                className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 justify-center items-center">
+              <p className="text-3xl center">회원가입</p>
+            </div>
+            <div
+                className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 justify-center items-center">
+              <p className="text-2xl center">Step 1</p>
+            </div>
+            <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+              <Input
+                  type="string"
+                  label="아이디"
+                  labelPlacement="outside"
+                  startContent={
+                    <EditIcon
+                        className="text-2xl text-default-400 pointer-events-none flex-shrink-0"/>
+                  }
+              />
+            </div>
+            <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+              <Input
+                  type="password"
+                  label="비밀번호"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  labelPlacement="outside"
+                  startContent={
+                    <LockFilledIcon
+                        className="text-2xl text-default-400 pointer-events-none flex-shrink-0"/>
+                  }
+              />
+            </div>
+            <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+              <Input
+                  type="password"
+                  label="비밀번호 재확인"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  labelPlacement="outside"
+                  startContent={
+                    <LockFilledIcon
+                        className="text-2xl text-default-400 pointer-events-none flex-shrink-0"/>
+                  }
+              />
+            </div>
+
+            <div
+                className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 justify-center items-center">
+              <Button color="secondary" fullWidth={true} onClick={prevStep}>
+                이전 단계로
+              </Button>
+              <Button color="secondary" fullWidth={true} onClick={nextStep}>
+                다음 단계로
+              </Button>
+            </div>
+          </div>
+      );
+    case 2:
+      return (
+          <div className="flex flex-col gap-4 p-6 bg-top rounded-lg shadow-lg">
+            <div
+                className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 justify-center items-center">
+              <p className="text-3xl center">회원가입</p>
+            </div>
+            <div
+                className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 justify-center items-center">
+              <p className="text-2xl center">Step 2</p>
+            </div>
+            <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+              <Input
+                  type="string"
+                  label="이름"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  labelPlacement="outside"
+                  startContent={
+                    <AvatarIcon
+                        className="text-2xl text-default-400 pointer-events-none flex-shrink-0"/>
+                  }
+              />
+            </div>
+            <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+              <Input
+                  type="email"
+                  label="이메일"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  labelPlacement="outside"
+                  startContent={
+                    <MailIcon
+                        className="text-2xl text-default-400 pointer-events-none flex-shrink-0"/>
+                  }
+              />
+            </div>
+            <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+              <Input
+                  type="number"
+                  label="휴대전화"
+                  value={number}
+                  onChange={(e) => setNumber(e.target.value)}
+                  labelPlacement="outside"
+                  startContent={
+                    <MonitorMobileIcon
+                        className="text-2xl text-default-400 pointer-events-none flex-shrink-0"/>
+                  }
+              />
+            </div>
+            <div
+                className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 justify-center items-center">
+              <Button color="secondary" fullWidth={true} onClick={prevStep}>
+                이전 단계로
+              </Button>
+              <Button color="secondary" fullWidth={true} onClick={nextStep}>
+                다음 단계로
+              </Button>
+            </div>
+          </div>
+      );
+    case 3:
+      return (
+          <div className="flex flex-col gap-4 p-6 bg-top rounded-lg shadow-lg">
+            <div
+                className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 justify-center items-center">
+              <p className="text-3xl center">회원가입</p>
+            </div>
+            <div
+                className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 justify-center items-center">
+              <p className="text-2xl center">Step 3</p>
+            </div>
+            <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+              <CheckboxGroup
+                  className="gap-5"
+                  label="선호하는 장르"
+                  orientation="horizontal"
+                  value={selectedGenres}
+                  onValueChange={setSelectedGenres}
+              >
+                <Checkbox value="action">액션</Checkbox>
+                <Checkbox value="drama">드라마</Checkbox>
+                <Checkbox value="comedy">코미디</Checkbox>
+                <Checkbox value="romance">로맨스</Checkbox>
+                <Checkbox value="history">사극</Checkbox>
+              </CheckboxGroup>
+            </div>
+            <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+              <Listbox
+                  className="gap-5"
+                  aria-label="Favorite Artist"
+                  variant="flat"
+                  disallowEmptySelection
+                  selectionMode="multiple"
+                  selectedKeys={selectedArtist}
+                  onSelectionChange={(keys) => setSelectedArtist(keys as Set<string>)}
+              >
+                <ListboxItem key="start1">BTS</ListboxItem>
+                <ListboxItem key="start2">BLACKPINK</ListboxItem>
+                <ListboxItem key="start3">NEWJEANS</ListboxItem>
+              </Listbox>
+            </div>
+            <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+              <Spacer y={1}/>
+            </div>
+            <div
+                className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 justify-center items-center">
+              <Button color="secondary" fullWidth={true} onClick={prevStep}>
+                이전 단계로
+              </Button>
+              <Button color="secondary" fullWidth={true} onClick={handleSubmit}>
+                회원가입
+              </Button>
+            </div>
+            <div
+                className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 justify-center items-center">
+              <SunFilledIcon
+                  className="text-2xl text-default-400 pointer-events-none flex-shrink-0"/>
+              <SunFilledIcon
+                  className="text-2xl text-default-400 pointer-events-none flex-shrink-0"/>
+              <SunFilledIcon
+                  className="text-2xl text-default-400 pointer-events-none flex-shrink-0"/>
+            </div>
+          </div>
+      );
+    default:
+      return null;
+  }
 }
