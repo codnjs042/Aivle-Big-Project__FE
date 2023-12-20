911a5e53f76f69218b4933922ba2c23a3798a5b4
ChoHyeonWoo <hwcho0456@gmail.com>
2023-12-20 16:33:31 +0900

    fix: change login register button and footer

--- components/layouts/footer.tsx
+++ components/layouts/footer.tsx
@@ -1,24 +1,30 @@
 import {Link} from "@nextui-org/link";
 import TermOfUse from "../modals/termOfUse";
 import PrivacyPolicy from "../modals/privacyPolicy";
+import {Chip} from "@nextui-org/chip";
 
 export default function Footer() {
   return (
-      <footer className="w-full flex items-center justify-around py-5">
-        <div className="flex flex-row gap-1">
+      <footer className="w-full flex items-center justify-evenly py-5">
+        <div className="flex flex-row gap-5">
           <PrivacyPolicy/>
           <TermOfUse/>
         </div>
-        <Link
-            isExternal
-            className="flex items-center gap-1 text-current"
-            href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-            title="nextui.org homepage"
-        >
-          <span className="text-default-600">Powered by</span>
-          <p className="text-secondary">NextUI</p>
-        </Link>
-        <p>© 2023 AivleSchool 4기 21조. <strong>All rights reserved.</strong></p>
+        <Chip className="flex items-center gap-1 text-base">
+          <span className="text-default-600">© 2023 AivleSchool 4기 21조. <strong>All rights
+            reserved.</strong></span>
+        </Chip>
+        <Chip className="flex flex-row gap-1">
+          <Link
+              isExternal
+              className="flex items-center gap-1 text-current"
+              href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
+              title="nextui.org homepage"
+          >
+            <span className="text-default-600">Powered by</span>
+            <p className="text-secondary">NextUI</p>
+          </Link>
+        </Chip>
       </footer>
   );
 }
