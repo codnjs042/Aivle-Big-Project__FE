885f6be0a0d6f2a3978d86b6f42ca6f431d7ff2a
ChoHyeonWoo <hwcho0456@gmail.com>
2023-12-22 15:10:47 +0900

    fix: fix login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,38 +4,42 @@ import {ReactNode, useState} from "react";
 import {Button, Checkbox, Divider, Input, Spacer} from '@nextui-org/react';
 import {EditIcon, LockFilledIcon, SunFilledIcon} from "@nextui-org/shared-icons";
 import {Link} from "@nextui-org/link";
-import {redirect} from 'next/navigation'
+import { useRouter } from 'next/navigation'
 
 function Col(props: { span: number, children: ReactNode }) {
   return null;
 }
 
 export default function LoginPage() {
-  const [username, setUsername] = useState("");
+  const router = useRouter();
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const handleSubmit = async (e: { preventDefault: () => void; }) => {
     e.preventDefault();
     const dataToSend = {
-      email: username,
+      email: email,
       password: password,
     };
-
+    console.log('email : ' + email);
+    console.log('password : ' + password);
     try {
-      const response = await fetch('http://localhost:8000/api/hello/', {
-        method: 'GET',
+      const response = await fetch('http://localhost:8000/api/token/', {
+        method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        //body: JSON.stringify(dataToSend),
+        body: JSON.stringify(dataToSend),
       });
-
       if (!response.ok) {
-        throw new Error('서버에서 문제가 발생했습니다. 에러 코드: ' + response.status);
+        if (response.status === 400) {
+          throw new Error('이메일 또는 비밀번호가 일치하지 않습니다.');
+        }
+        throw new Error('서버에서 문제가 발생했습니다. 에러 코드: ' + response.status + response.statusText);
       }
-
       const data = await response.json();
       console.log(data);
-      //redirect('/');
+      console.log('redirect to /');
+      router.replace('/');
     } catch (error) {
       console.error('로그인 요청 중 문제가 발생했습니다: ', error);
     }
@@ -49,12 +53,14 @@ export default function LoginPage() {
         </div>
         <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
           <Input
-              type="string"
-              label="ID"
+              type="email"
+              label="Email"
               labelPlacement="outside"
               startContent={
                 <EditIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0"/>
               }
+              value={email}
+              onChange={e => setEmail(e.target.value)}
           />
         </div>
         <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
@@ -66,6 +72,8 @@ export default function LoginPage() {
                 <LockFilledIcon
                     className="text-2xl text-default-400 pointer-events-none flex-shrink-0"/>
               }
+              value={password}
+              onChange={e => setPassword(e.target.value)}
           />
 
         </div>
