753d3ddd9539b1440264b58378eefdd405f2c545
ChoHyeonWoo <hwcho0456@gmail.com>
2023-12-22 16:27:56 +0900

    fix: fix login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -30,7 +30,7 @@ export default function LoginPage() {
         Cookies.set('refresh_token', data.refresh);
         router.replace('/');
       }
-      else if (response.status === 400) {
+      else if (response.status === 400 || response.status === 401) {
         throw new Error('이메일 또는 비밀번호가 일치하지 않습니다.');
       } else {
         throw new Error('서버에 문제가 생겨 관리자에게 문의하시기 바랍니다.');
