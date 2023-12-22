476e3cbb3d0188e5324ef32f24d89055cec7593b
ChoHyeonWoo <hwcho0456@gmail.com>
2023-12-22 14:28:25 +0900

    feat: add login api

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import {ReactNode, useState} from "react";
-import {Textarea, Input, Button, Spacer, Checkbox, Divider} from '@nextui-org/react';
-import {LockFilledIcon, MailIcon, SunFilledIcon, EditIcon} from "@nextui-org/shared-icons";
+import {Button, Checkbox, Divider, Input, Spacer} from '@nextui-org/react';
+import {EditIcon, LockFilledIcon, SunFilledIcon} from "@nextui-org/shared-icons";
 import {Link} from "@nextui-org/link";
+import {redirect} from 'next/navigation'
 
 function Col(props: { span: number, children: ReactNode }) {
   return null;
@@ -12,16 +13,36 @@ function Col(props: { span: number, children: ReactNode }) {
 export default function LoginPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [step, setStep] = useState(false);
-  const nextStep = () => setStep(step=>!step);
-  const handleSubmit = (e: { preventDefault: () => void; }) => {
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
     e.preventDefault();
+    const dataToSend = {
+      email: username,
+      password: password,
+    };
+
+    try {
+      const response = await fetch('http://localhost:8000/api/hello/', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        //body: JSON.stringify(dataToSend),
+      });
+
+      if (!response.ok) {
+        throw new Error('서버에서 문제가 발생했습니다. 에러 코드: ' + response.status);
+      }
+
+      const data = await response.json();
+      console.log(data);
+      //redirect('/');
+    } catch (error) {
+      console.error('로그인 요청 중 문제가 발생했습니다: ', error);
+    }
   };
 
-  switch (step) {
-    case false:
-      return (
-        <div className="flex flex-col gap-4 p-6 bg-top rounded-lg shadow-lg">
+  return (
+      <div className="flex flex-col gap-4 p-6 bg-top rounded-lg shadow-lg">
         <div
             className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 justify-center items-center">
           <p className="text-3xl center">Welcome !</p>
@@ -42,7 +63,8 @@ export default function LoginPage() {
               label="Password"
               labelPlacement="outside"
               startContent={
-                <LockFilledIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0"/>
+                <LockFilledIcon
+                    className="text-2xl text-default-400 pointer-events-none flex-shrink-0"/>
               }
           />
 
@@ -52,14 +74,14 @@ export default function LoginPage() {
         </div>
         <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
           <Checkbox defaultSelected color="secondary">아이디 기억하기</Checkbox>
-          <Link color="secondary" onClick={nextStep}>비밀번호를 잊으셨나요?</Link>
+          <Link color="secondary">비밀번호를 잊으셨나요?</Link>
         </div>
         <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
           <Spacer y={1}/>
         </div>
         <div
             className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 justify-center items-center">
-          <Button color="secondary" fullWidth={true}>
+          <Button color="secondary" fullWidth={true} onClick={handleSubmit}>
             로그인
           </Button>
         </div>
@@ -70,10 +92,5 @@ export default function LoginPage() {
           <SunFilledIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0"/>
         </div>
       </div>
-      );
-    case true:
-      return (
-          <button onClick={nextStep}>창닫기</button>
-      );
-  }
+  );
 }
