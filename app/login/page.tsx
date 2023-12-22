c8c3c8e4a44c91c9ff0915f05cd126ea84cf23cc
ChoHyeonWoo <hwcho0456@gmail.com>
2023-12-22 16:23:37 +0900

    feat: add login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,44 +4,40 @@ import {ReactNode, useState} from "react";
 import {Button, Checkbox, Divider, Input, Spacer} from '@nextui-org/react';
 import {EditIcon, LockFilledIcon, SunFilledIcon} from "@nextui-org/shared-icons";
 import {Link} from "@nextui-org/link";
-import { useRouter } from 'next/navigation'
-
-function Col(props: { span: number, children: ReactNode }) {
-  return null;
-}
+import { useRouter } from 'next/navigation';
+import Cookies from 'js-cookie';
 
 export default function LoginPage() {
   const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const handleSubmit = async (e: { preventDefault: () => void; }) => {
-    e.preventDefault();
-    const dataToSend = {
-      email: email,
-      password: password,
-    };
-    console.log('email : ' + email);
-    console.log('password : ' + password);
+  const [errorMessage, setErrorMessage] = useState('');
+  const handleSubmit = async () => {
     try {
       const response = await fetch('http://localhost:8000/api/token/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(dataToSend),
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
       });
-      if (!response.ok) {
-        if (response.status === 400) {
-          throw new Error('이메일 또는 비밀번호가 일치하지 않습니다.');
-        }
-        throw new Error('서버에서 문제가 발생했습니다. 에러 코드: ' + response.status + response.statusText);
+      if (response.ok) {
+        const data = await response.json();
+        Cookies.set('access_token', data.access);
+        Cookies.set('refresh_token', data.refresh);
+        router.replace('/');
       }
-      const data = await response.json();
-      console.log(data);
-      console.log('redirect to /');
-      router.replace('/');
+      else if (response.status === 400) {
+        throw new Error('이메일 또는 비밀번호가 일치하지 않습니다.');
+      } else {
+        throw new Error('서버에 문제가 생겨 관리자에게 문의하시기 바랍니다.');
+      }
     } catch (error) {
-      console.error('로그인 요청 중 문제가 발생했습니다: ', error);
+      if (error instanceof Error)
+        setErrorMessage(error.message);
     }
   };
 
@@ -92,6 +88,11 @@ export default function LoginPage() {
           <Button color="secondary" fullWidth={true} onClick={handleSubmit}>
             로그인
           </Button>
+        </div>
+        <div
+            className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 justify-center items-center"
+            style={{ height: '50px' }}>
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
         </div>
         <div
             className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4 justify-center items-center">
