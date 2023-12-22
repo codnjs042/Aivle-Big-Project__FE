779ed3147f79c894393a17317279b8367d246716
jeongan <ch3843@naver.com>
2023-12-22 12:46:18 +0900

    learn

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,9 +1,38 @@
+"use client";
 import { title } from "@/components/primitives";
-
+import React from "react";
+import {Tabs, Tab, Card, CardBody, CardHeader} from "@nextui-org/react";
 export default function LearnPage() {
-	return (
-		<div>
-			<h1 className={title()}>한국어를 배워보세요!</h1>
-		</div>
+	let tabs = [
+		{
+			id: "한국어",
+			label: "한국어",
+			content: "안녕하세요[안녕하세요]"
+		},
+		{
+			id: "영어발음",
+			label: "영어발음",
+			content: "Annyunghaseyo"
+		},
+		{
+			id: "영어",
+			label: "영어",
+			content: "Hello"
+		}
+		];
+		return (
+			<div className="flex w-full flex-col">
+		<Tabs aria-label="Dynamic tabs" items={tabs}>
+			{(item) => (
+			<Tab key={item.id} title={item.label}>
+				<Card>
+				<CardBody>
+					{item.content}
+				</CardBody>
+				</Card>  
+			</Tab>
+			)}
+		</Tabs>
+		</div> 
 	);
 }
