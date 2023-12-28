a06595ac865fbf84b902beebfc1f9a775a12f524
jeongan <ch3843@naver.com>
2023-12-28 17:16:02 +0900

    lyrics card

--- app/lyrics/page.tsx
+++ app/lyrics/page.tsx
@@ -141,85 +141,42 @@ export default function PracticePage() {
       className="border-none bg-background/60 dark:bg-default-100/50 max-w-[1000px]"
       shadow="sm"
     >
-      <CardBody style={{ marginLeft:'50px'}}>
-        <div className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-20 items-center justify-center">
-          <div className="relative col-span-6 md:col-span-4">
-          <Image
+      <CardBody style={{paddingTop:'50px', paddingBottom:'50px', paddingLeft:'50px', paddingRight:'50px'}}>
+          <div className="grid grid-cols-6 md:grid-cols-3 gap-6 md:gap-20 items-center justify-center">
+            <div className="flex flex-col col-span-6 md:col-span-8">
+              {/* 사진, 문장, 하트 */}
+              <div className="flex justify-between items-center">
+                <div className="relative col-span-6 md:col-span-4 mr-10 ">
+                <Image
                   alt="Album cover"
                   className="object-cover"
                   height={200}
                   shadow="md"
                   src={selectedArtist ? artistInfo[selectedArtist]?.image || undefined : undefined}
-                  width="100%"
+                  width="250"
                 />
-          </div>
-
-          <div className="flex flex-col col-span-6 md:col-span-7">
-            <div className="flex justify-between items-start">
-              <div className="flex flex-col gap-0">
-                <p className="text-small text-foreground/80 mt-5">{selectedArtist && artistInfo[selectedArtist]?.text1}</p>
-				        <h3 className="font-semibold text-foreground/90 mt-5">{selectedArtist && artistInfo[selectedArtist]?.text2}</h3>
-                <h1 className="text-large font-medium mt-5">{selectedArtist && artistInfo[selectedArtist]?.text3}</h1>
-              </div>
-              <Button
-                isIconOnly
-                className="text-default-900/60 data-[hover]:bg-foreground/10 -translate-y-2 translate-x-2"
-                radius="full"
-                variant="light"
-                onPress={() => setLiked((v) => !v)}
-              >
+                </div>
+                {/* 단어 문장 */}
+                <div className="flex flex-col gap-2">
+                  <h1 className="text-large text-foreground/80 mt-5">{selectedArtist && artistInfo[selectedArtist]?.text1}</h1>
+                  <h1 className="font-large text-foreground/90 mt-5">{selectedArtist && artistInfo[selectedArtist]?.text2}</h1>
+                  <h1 className="text-large font-medium mt-5">{selectedArtist && artistInfo[selectedArtist]?.text3}</h1>
+                </div>
+                <Button
+                  isIconOnly
+                  className="text-default-900/60 data-[hover]:bg-foreground/10 -translate-y-2 translate-x-2"
+                  radius="full"
+                  variant="light"
+                  onPress={() => setLiked((v) => !v)}
+                >
                 <HeartIcon
-                  className={liked ? "[&>path]:stroke-transparent" : ""}
-                  fill={liked ? "currentColor" : "none"}
-                />
-              </Button>
-            </div>
-
-            <div className="flex flex-col mt-20 gap-1">
-              <Slider
-                aria-label="Record progress"
-                classNames={{
-                  track: "bg-default-500/30",
-                  thumb: "w-2 h-2 after:w-2 after:h-2 after:bg-foreground",
-                }}
-                color="foreground"
-                defaultValue={33}
-                size="sm"
-              />
-              <div className="flex justify-between">
-                <p className="text-small">0:04</p>
-                <p className="text-small text-foreground/50">0:10</p>
+                    className={liked ? "[&>path]:stroke-transparent" : ""}
+                    fill={liked ? "currentColor" : "none"}
+                  />
+                </Button>
               </div>
             </div>
-
-            <div className="flex w-full items-center justify-center">
-              <Button
-                isIconOnly
-                className="data-[hover]:bg-foreground/10"
-                radius="full"
-                variant="light"
-              >
-                <PreviousIcon />
-              </Button>
-              <Button
-                isIconOnly
-                className="w-auto h-auto data-[hover]:bg-foreground/10"
-                radius="full"
-                variant="light"
-              >
-                <PauseCircleIcon size={54} />
-              </Button>
-              <Button
-                isIconOnly
-                className="data-[hover]:bg-foreground/10"
-                radius="full"
-                variant="light"
-              >
-                <NextIcon />
-              </Button>
-            </div>
           </div>
-        </div>
       </CardBody>
     </Card>
     <Card
