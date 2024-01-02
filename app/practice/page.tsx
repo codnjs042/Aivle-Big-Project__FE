70c5053d1b67abdf1b19b174f198ed1a1ae6daf2
codnjs <codnjs042@naver.com>
2024-01-02 17:18:53 +0900

    k-contents part

--- app/practice/page.tsx
+++ app/practice/page.tsx
@@ -15,7 +15,9 @@ export default function PracticePage() {
   const contentTitle = queryParams.get('content');
   const content = contentsList.find(item => item.title === contentTitle);
   const [liked, setLiked] = React.useState(false);
-  
+  const [selectedGenre, setSelectedGenre] = useState<string>('');
+  const [selectedPart, setSelectedPart] = useState(1);
+
   // 음성녹음용 오디오
   const [voice, setVoice] = useState<string | null>(null);
   const [voiceUrl, setVoiceUrl] = useState<string | null>(null);
@@ -86,158 +88,146 @@ export default function PracticePage() {
   if (!content) {
     return null;
   }
+
+  const goToGenrePage = () => {
+    router.push('/genre');
+  };
+
+   const renderPart = (partNumber:any) => {
+    type practice = /*unresolved*/ any
+    const partData = (content as practice)[`part${partNumber}`];
+
   return (
-    <div>
+    <div key={partNumber}>
     <Card
       isBlurred
       className="border-none bg-background/60 dark:bg-default-100/50 max-w-[1000px]"
       shadow="sm"
     >
-      <CardBody style={{ marginLeft:'50px'}}>
-        <div className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-20 items-center justify-center">
-          <div className="relative col-span-6 md:col-span-4">
-          <ReactPlayer
-            url={content.video}
-            width="100%"
-            height="200px"
-            controls={true}
-            onError={(e) => console.error('Video error', e)}
-          />
-          </div> 
-          <div className="flex flex-col col-span-6 md:col-span-7">
-            <div className="flex justify-between items-start">
-              <div className="flex flex-col gap-0">
-                <h1 className="font-large text-foreground/80 mt-5">{content.english}</h1>
-				        <h1 className="text-large text-foreground/90 mt-5">{content.pronun}</h1>
-                <h1 className="text-large font-medium mt-5">{content.contents}</h1>
-              </div>
-              <Button
-                isIconOnly
-                className="text-default-900/60 data-[hover]:bg-foreground/10 -translate-y-2 translate-x-2"
-                radius="full"
-                variant="light"
-                onPress={() => setLiked((v) => !v)}
-              >
-                <HeartIcon
-                  className={liked ? "[&>path]:stroke-transparent" : ""}
-                  fill={liked ? "currentColor" : "none"}
-                />
-              </Button>
-            </div>
-
-            <div className="flex flex-col mt-20 gap-1">
-              <Slider
-                aria-label="Record progress"
-                classNames={{
-                  track: "bg-default-500/30",
-                  thumb: "w-2 h-2 after:w-2 after:h-2 after:bg-foreground",
-                }}
-                color="foreground"
-                defaultValue={33}
-                size="sm"
-              />
-              <div className="flex justify-between">
-                <p className="text-small">0:04</p>
-                <p className="text-small text-foreground/50">0:10</p>
+      <CardBody style={{ paddingTop: '50px', paddingBottom: '50px', paddingLeft: '50px', paddingRight: '50px' }}>
+            <div className="grid grid-cols-6 md:grid-cols-3 gap-6 md:gap-20 items-center justify-center">
+              <div className="flex flex-col col-span-6 md:col-span-8">
+                {/* 사진, 문장, 하트 */}
+                <div className="flex justify-between items-center">
+                  <div className="relative col-span-6 md:col-span-4 mr-10 ">
+                    {partData ? partData[3] : undefined}
+                  </div>
+                  {/* 단어 문장 */}
+                  <div className="flex flex-col gap-2">
+                    <h1 className="font-large text-foreground/80 mt-5">{partData ? partData[1] : undefined}</h1>
+                    <h1 className="text-large text-foreground/90 mt-5">{partData ? partData[2] : undefined}</h1>
+                    <h1 className="text-large font-medium mt-5">{partData ? partData[0] : undefined}</h1>
+                  </div>
+                  <Button
+                    isIconOnly
+                    className="text-default-900/60 data-[hover]:bg-foreground/10 -translate-y-2 translate-x-2"
+                    radius="full"
+                    variant="light"
+                    onPress={() => setLiked((v) => !v)}
+                  >
+                    <HeartIcon
+                      className={liked ? "[&>path]:stroke-transparent" : ""}
+                      fill={liked ? "currentColor" : "none"}
+                    />
+                  </Button>
+                </div>
               </div>
             </div>
+          </CardBody>
+        </Card>
 
-            <div className="flex w-full items-center justify-center">
-              <Button
-                isIconOnly
-                className="data-[hover]:bg-foreground/10"
-                radius="full"
-                variant="light"
-              >
-                <PreviousIcon />
-              </Button>
-              <Button
-                isIconOnly
-                className="w-auto h-auto data-[hover]:bg-foreground/10"
-                radius="full"
-                variant="light"
-              >
-                <PauseCircleIcon size={54} />
-              </Button>
-              <Button
-                isIconOnly
-                className="data-[hover]:bg-foreground/10"
-                radius="full"
-                variant="light"
-              >
-                <NextIcon />
-              </Button>
+        <Card
+          isBlurred
+          className="border-none bg-background/60 dark:bg-default-100/50 max-w-[1000px] mt-10"
+          shadow="sm"
+        >
+        <div className="flex flex-col">
+            <div className="text-3xl font-bold primary text-center py-5">
+                <Button
+                  isIconOnly
+                  className="w-20 item-center ml-5"
+                  color="secondary" 
+                  variant="ghost"
+                  onPress={recording ? stopRecording : startRecording}
+                  >
+                  {recording ? "녹음정지" : "녹음시작"}
+                </Button>
+                <Button
+                  isIconOnly
+                  className="w-20 item-center ml-5"
+                  color="secondary" 
+                  variant="ghost"
+                  onPress={playRecording}
+                  >
+                  음성듣기
+                </Button>
+                {voice && <audio controls src={voice} />}
+                <Button 
+                    onClick={handleAnalysis}
+                    className="w-20 item-center ml-5"
+                    color="secondary" 
+                    variant="ghost">발음 분석
+              </Button> 
             </div>
           </div>
-        </div>
-      </CardBody>
-    </Card>
-    <Card
-      isBlurred
-      className="border-none bg-background/60 dark:bg-default-100/50 max-w-[1000px] mt-10"
-      shadow="sm"
-    >
-     <div className="flex flex-col">
-        <div className="text-3xl font-bold primary text-center py-5">
-            <Button
-              isIconOnly
-              className="w-20 item-center ml-5"
-              color="secondary" 
-              variant="ghost"
-              onPress={recording ? stopRecording : startRecording}
-              >
-              {recording ? "녹음정지" : "녹음시작"}
-            </Button>
-            <Button
-              isIconOnly
-              className="w-20 item-center ml-5"
-              color="secondary" 
-              variant="ghost"
-              onPress={playRecording}
-              >
-              음성듣기
-            </Button>
-            {voice && <audio controls src={voice} />}
-            <Button 
-                onClick={handleAnalysis}
-                className="w-20 item-center ml-5"
-                color="secondary" 
-                variant="ghost">발음 분석
-          </Button> 
-        </div>
+        </Card>
+
+        <Card
+          isBlurred
+          className={cn("border-none bg-background/60 dark:bg-default-100/50 max-w-[1000px] mt-10", {
+            hidden: !AnalysisVisible, // 숨겨진 상태
+          })}
+          shadow="sm"
+        >
+          <CardHeader className="flex gap-3">
+            <Logo />
+            <div className="flex flex-col">
+              <p className="text-md font-bold">AI 레포트</p>
+            </div>
+          </CardHeader>
+          <Divider />
+          <CardBody>
+            <p>원래 발음 : 안녕하세요</p>
+          </CardBody>
+          <CardBody>
+            <p>나의 발음 : 안녕하세요</p>
+          </CardBody>
+          <Divider />
+          <CardFooter>
+            <Link color="secondary" showAnchorIcon href="/mypage">
+              더 자세한 AI 레포트
+            </Link>
+          </CardFooter>
+        </Card>
       </div>
-      </Card>
-      <Card
-        isBlurred
-        className={cn("border-none bg-background/60 dark:bg-default-100/50 max-w-[1000px] mt-10", {
-          hidden: !AnalysisVisible, // 숨겨진 상태
-        })}
-        shadow="sm"
-      >
-        <CardHeader className="flex gap-3">
-          <Logo />
-          <div className="flex flex-col">
-            <p className="text-md font-bold">AI 레포트</p>
-          </div>
-        </CardHeader>
-        <Divider/>
-        <CardBody>
-          <p>원래 발음 : 안녕하세요</p>
-        </CardBody>
-        <CardBody>
-          <p>나의 발음 : 안녕하세요</p>
-        </CardBody>
-        <Divider/>
-        <CardFooter>
-          <Link
-            color="secondary"
-            showAnchorIcon
-            href="/mypage"
+    );
+  };
+
+  return (
+    <div>
+      <div className="mt-5 flex justify-center">
+        <Button
+          variant="light"
+          color="default"
+          onPress={goToGenrePage}
+          disabled
+        >
+          홈
+        </Button>
+        {Array.from({ length: 3 }, (_, i) => i + 1).map((partNumber) => (
+          <Button
+            key={partNumber}
+            variant="light"
+            color={selectedPart === partNumber ? "secondary" : "default"}
+            onPress={() => setSelectedPart(partNumber)}
+            className="ml-3"
           >
-            더 자세한 AI 레포트 
-          </Link>
-        </CardFooter>
-      </Card>
+            파트 {partNumber}
+          </Button>
+        ))}
+      </div>
+
+      {renderPart(selectedPart)}
     </div>
   );
 }
