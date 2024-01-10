59c64e07f7c98019680e8d2255df5c5d8e716c39
ChoHyeonWoo <hwcho0456@gmail.com>
2024-01-10 19:05:39 +0900

    merge

--- app/lyrics/page.tsx
+++ app/lyrics/page.tsx
@@ -1,117 +1,35 @@
 "use client"
 
-import React, {useContext, useState, useRef, useEffect} from "react";
+import React, {useContext, useEffect, useRef, useState} from "react";
 import AuthContext from "@/context/AuthContext";
 import dynamic from 'next/dynamic';
-import {CircularProgress, Chip, Card, CardBody, CardHeader, CardFooter, Divider, Link, Image, Button, Slider} from "@nextui-org/react";
-import {Logo, HeartIcon, PauseCircleIcon, NextIcon, PreviousIcon} from "@/components/icons";
-import { useRouter, useSearchParams } from "next/navigation";
-import {Artist, artistList} from "@/types/artist";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Chip,
+  CircularProgress,
+  Divider,
+  Link
+} from "@nextui-org/react";
+import {HeartIcon, Logo} from "@/components/icons";
+import {useRouter, useSearchParams} from "next/navigation";
+import {artistList} from "@/types/artist";
 import lyricsList from '../../public/data/lyrics';
 import cn from 'classnames';
+import Player from "@/components/layouts/player";
 
 export default function PracticePage() {
-  const auth = useContext(AuthContext);
   const router = useRouter();
   const queryParams = useSearchParams();
   const artist = queryParams.get('artist');
   const lyrics = lyricsList.find(item => item.title === artist);
   const [liked, setLiked] = React.useState(false);
-  const DynamicReactPlayer = dynamic(() => import('react-player'), { ssr: false });
+  const DynamicReactPlayer = dynamic(() => import('react-player'), {ssr: false});
   const [selectedArtist, setSelectedArtist] = useState<string>('');
   const [selectedPart, setSelectedPart] = useState(1);
-
-  // 음성녹음용 오디오
-  const [voice, setVoice] = useState<string | null>(null);
-  const [voiceUrl, setVoiceUrl] = useState<string | null>(null);
-  // 녹음 상태 및 녹음된 Blob을 저장할 상태
-  const [recording, setRecording] = useState(false);
-  const [recordedBlob, setRecordedBlob] = useState<Blob | null>(null);
-
-  // 미디어 스트림 및 녹음기 참조
-  const mediaStreamRef = useRef<MediaStream | null>(null);
-  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
-
-  
-  const [AnalysisVisible, setAnalysisVisible] = useState(false);
-
-  // 음성 녹음 시작 함수
-  const startRecording = async () => {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      mediaStreamRef.current = stream;
-      const recorder = new MediaRecorder(stream);
-      mediaRecorderRef.current = recorder;
-
-      const chunks: BlobPart[] | undefined = [];
-
-      recorder.ondataavailable = (event) => {
-        if (event.data.size > 0) {
-          chunks.push(event.data);
-        }
-      };
-      
-      recorder.onstop = () => {
-        const blob = new Blob(chunks, { type: 'audio/wav' });
-        setRecordedBlob(blob);
-
-        const url = URL.createObjectURL(blob);
-        setVoiceUrl(url);
-      };
-
-      recorder.start();
-      setRecording(true);
-    } catch (error) {
-      console.error('Error starting recording:', error);
-    }
-  };
-
-  // 음성 녹음 종료 함수
-  const stopRecording = () => {
-    if (mediaRecorderRef.current && recording) {
-      mediaRecorderRef.current.stop();
-      mediaStreamRef.current?.getTracks().forEach((track: MediaStreamTrack) => track.stop());
-      setRecording(false);
-    }
-  };
-
-    // 음성 녹음 종료 및 저장 함수
-    const stopRecordingAndSave = () => {
-      stopRecording(); // 녹음 중지
-      if (recordedBlob) {
-        // 녹음된 Blob이 존재하면 WAV 파일로 저장
-        const blob = new Blob([recordedBlob], { type: 'audio/wav' });
-        const url = URL.createObjectURL(blob);
-        setVoiceUrl(url);
-      }
-    };
-  
-    const downloadRecording = (url: string | null, filename: string) => {
-      const anchor = document.createElement('a');
-      if (url!=null)
-        anchor.href = url;
-      anchor.download = filename;
-      anchor.click();
-    };
-
-  // 음성 녹음 듣기 함수
-  const playRecording = () => {
-    if (voiceUrl) {
-      setVoice((prevVoice) => (prevVoice === voiceUrl ? null : voiceUrl));
-      const voice = new Audio(voiceUrl);
-      // voice.play();
-    }
-  };
-
-
-  // 발음 분석 버튼 클릭 시 호출
-  const handleAnalysis = () => {
-    if (auth.login) {
-      setAnalysisVisible((prevVisible) => !prevVisible);
-    } else {
-      alert("로그인이 필요합니다.");
-    }
-  };
 
   const goToArtistPage = () => {
     router.push('/artist');
@@ -127,243 +45,92 @@ export default function PracticePage() {
   }, [artist]);
 
 
-  const renderPart = (partNumber:any) => {
+  const renderPart = (partNumber: any) => {
     type Lyrics = /*unresolved*/ any
     const partData = (lyrics as Lyrics)[`part${partNumber}`];
 
     return (
-      <div key={partNumber}>
-        <Card
-          isBlurred
-          className="border-none bg-background/60 dark:bg-default-100/50 max-w-[1500px]"
-          shadow="sm"
-        >
-          <CardBody style={{ paddingTop: '50px', paddingBottom: '50px', paddingLeft: '50px', paddingRight: '50px' }}>
-            <div className="grid grid-cols-6 md:grid-cols-3 gap-6 md:gap-20 items-center justify-center">
-              <div className="flex flex-col col-span-6 md:col-span-8">
-                {/* 사진, 문장, 하트 */}
-                <div className="flex justify-between items-center">
-                  <div className="relative col-span-6 md:col-span-4 mr-10 ">
-                    {partData && partData[3] ? (
-                    <iframe
-                    width="560"
-                    height="315"
-                    src={partData[3]}
-                    title="YouTube video player"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  ></iframe>
-              ) : undefined}
+        <div key={partNumber}>
+          <Card
+              isBlurred
+              className="border-none bg-background/60 dark:bg-default-100/50 max-w-[1500px]"
+              shadow="sm"
+          >
+            <CardBody style={{
+              paddingTop: '50px',
+              paddingBottom: '50px',
+              paddingLeft: '50px',
+              paddingRight: '50px'
+            }}>
+              <div
+                  className="grid grid-cols-6 md:grid-cols-3 gap-6 md:gap-20 items-center justify-center">
+                <div className="flex flex-col col-span-6 md:col-span-8">
+                  {/* 사진, 문장, 하트 */}
+                  <div className="flex justify-between items-center">
+                    <div className="relative col-span-6 md:col-span-4 mr-10 ">
+                      {partData && partData[3] ? (
+                          <iframe
+                              width="560"
+                              height="315"
+                              src={partData[3]}
+                              title="YouTube video player"
+                              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                          ></iframe>
+                      ) : undefined}
+                    </div>
+                    {/* 단어 문장 */}
+                    <div className="flex flex-col gap-2">
+                      <h1 className="font-large text-foreground/80 mt-5">{partData ? partData[1] : undefined}</h1>
+                      <h1 className="text-large text-foreground/90 mt-5">{partData ? partData[2] : undefined}</h1>
+                      <h1 className="text-large font-medium mt-5">{partData ? partData[0] : undefined}</h1>
+                    </div>
+                    <Button
+                        isIconOnly
+                        className="text-default-900/60 data-[hover]:bg-foreground/10 -translate-y-2 translate-x-2 ml-2"
+                        radius="full"
+                        variant="light"
+                        onPress={() => setLiked((v) => !v)}
+                    >
+                      <HeartIcon
+                          className={liked ? "[&>path]:stroke-transparent" : ""}
+                          fill={liked ? "currentColor" : "none"}
+                      />
+                    </Button>
                   </div>
-                  {/* 단어 문장 */}
-                  <div className="flex flex-col gap-2">
-                    <h1 className="font-large text-foreground/80 mt-5">{partData ? partData[1] : undefined}</h1>
-                    <h1 className="text-large text-foreground/90 mt-5">{partData ? partData[2] : undefined}</h1>
-                    <h1 className="text-large font-medium mt-5">{partData ? partData[0] : undefined}</h1>
-                  </div>
-                  <Button
-                    isIconOnly
-                    className="text-default-900/60 data-[hover]:bg-foreground/10 -translate-y-2 translate-x-2 ml-2"
-                    radius="full"
-                    variant="light"
-                    onPress={() => setLiked((v) => !v)}
-                  >
-                    <HeartIcon
-                      className={liked ? "[&>path]:stroke-transparent" : ""}
-                      fill={liked ? "currentColor" : "none"}
-                    />
-                  </Button>
                 </div>
               </div>
-            </div>
-          </CardBody>
-        </Card>
-
-        <Card
-          isBlurred
-          className="border-none bg-background/60 dark:bg-default-100/50 max-w-[1500px] mt-10 h-[160px]"
-          shadow="sm"
-        >
-        <div className="flex flex-col items-center">
-          <div className="text-3xl font-bold primary text-center py-5">
-            <Button
-              isIconOnly
-              className="w-20 item-center ml-5"
-              color="secondary" 
-              variant={recording ? undefined : "ghost"}
-              onPress={recording ? stopRecordingAndSave : startRecording}
-            >
-              {recording ? "녹음정지" : "녹음시작"}
-            </Button>
-            <Button
-              isIconOnly
-              className="w-20 item-center ml-5"
-              color="secondary" 
-              variant="ghost"
-              onPress={playRecording}
-            >
-              음성듣기
-            </Button>
-            <Button
-              isIconOnly
-              className="w-20 item-center ml-5"
-              color="secondary"
-              variant="ghost"
-              onPress={() => downloadRecording(voiceUrl, 'recorded_audio.wav')}
-            >
-              다운로드
-            </Button>
-            <Button 
-              onClick={handleAnalysis}
-              className="w-20 item-center ml-5"
-              color="secondary" 
-              variant={AnalysisVisible ? undefined : "ghost"}>
-              발음 분석
-            </Button>
-          </div>
-          {voice && <div className="flex justify-center"><audio controls src={voice} /></div>}
+            </CardBody>
+          </Card>
+         <Player answer={partData ? partData[partNumber] : undefined} />
         </div>
-      </Card>
-
-      <Card
-        isBlurred
-        className={cn("border-none bg-background/60 dark:bg-default-100/50 max-w-[1500px] mt-10", {
-          hidden: !AnalysisVisible, // 숨겨진 상태
-        })}
-        shadow="sm"
-      >
-        <CardHeader className="flex gap-3">
-        <Logo />
-          <div className="flex flex-col">
-            <p className="text-md font-bold">AI 레포트</p>
-          </div>
-        </CardHeader>
-        <Divider/>
-        <div className="flex flex-row gap-10 mt-5 mb-5">
-          <Card
-              className="w-[200px] h-[200px] border-none bg-gradient-to-br from-violet-500 to-fuchsia-500">
-            <CardBody className="justify-center items-center pb-0 ">
-              <CircularProgress
-                  classNames={{
-                    svg: "w-36 h-36 drop-shadow-md",
-                    indicator: "stroke-white",
-                    track: "stroke-white/10",
-                    value: "text-3xl font-semibold text-white",
-                  }}
-                  value={68}
-                  strokeWidth={4}
-                  showValueLabel={true}
-              />
-            </CardBody>
-            <CardFooter className="justify-center items-center pt-0">
-              <Chip
-                  classNames={{
-                    base: "border-1 border-white/30",
-                    content: "text-white/90 text-small font-semibold",
-                  }}
-                  variant="bordered"
-              >
-                발음 유창성
-              </Chip>
-            </CardFooter>
-          </Card>
-          <Card
-              className="w-[200px] h-[200px] border-none bg-gradient-to-br from-violet-500 to-fuchsia-500">
-            <CardBody className="justify-center items-center pb-0">
-              <CircularProgress
-                  classNames={{
-                    svg: "w-36 h-36 drop-shadow-md",
-                    indicator: "stroke-white",
-                    track: "stroke-white/10",
-                    value: "text-3xl font-semibold text-white",
-                  }}
-                  value={88}
-                  strokeWidth={4}
-                  showValueLabel={true}
-              />
-            </CardBody>
-            <CardFooter className="justify-center items-center pt-0">
-              <Chip
-                  classNames={{
-                    base: "border-1 border-white/30",
-                    content: "text-white/90 text-small font-semibold",
-                  }}
-                  variant="bordered"
-              >
-                발음 숙련도
-              </Chip>
-            </CardFooter>
-          </Card>
-          <Card
-              className="w-[200px] h-[200px] border-none bg-gradient-to-br from-violet-500 to-fuchsia-500">
-            <CardBody className="justify-center items-center pb-0">
-              <CircularProgress
-                  classNames={{
-                    svg: "w-36 h-36 drop-shadow-md",
-                    indicator: "stroke-white",
-                    track: "stroke-white/10",
-                    value: "text-3xl font-semibold text-white",
-                  }}
-                  value={88}
-                  strokeWidth={4}
-                  showValueLabel={true}
-              />
-            </CardBody>
-            <CardFooter className="justify-center items-center pt-0">
-              <Chip
-                  classNames={{
-                    base: "border-1 border-white/30",
-                    content: "text-white/90 text-small font-semibold",
-                  }}
-                  variant="bordered"
-              >
-                이해 가능도
-              </Chip>
-            </CardFooter>
-          </Card>
-        </div>
-        <Divider/>
-        <CardBody>
-          <p>원래 발음 : {partData ? partData[0] : undefined}</p>
-        </CardBody>
-        <CardBody>
-          <p>나의 발음 : 안녕하세요</p>
-        </CardBody>
-        <Divider />
-        <CardFooter>
-          <Link color="secondary" showAnchorIcon href="/mypage">
-            더 자세한 AI 레포트
-          </Link>
-        </CardFooter>
-      </Card>
-    </div>
-  );
-};
+    );
+  };
 
   return (
-    <div>
-      <div className="mt-5 flex justify-center">
-        <Button
-          variant="light"
-          color="default"
-          onPress={goToArtistPage}
-          disabled
-        >
-          홈
-        </Button>
-        {Array.from({ length: 3 }, (_, i) => i + 1).map((partNumber) => (
+      <div>
+        <div className="mt-5 flex justify-center">
           <Button
-            key={partNumber}
-            variant="light"
-            color={selectedPart === partNumber ? "secondary" : "default"}
-            onPress={() => setSelectedPart(partNumber)}
-            className="ml-3"
+              variant="light"
+              color="default"
+              onPress={goToArtistPage}
+              disabled
           >
-            파트 {partNumber}
+            홈
           </Button>
-        ))}
-      </div>
+          {Array.from({length: 3}, (_, i) => i + 1).map((partNumber) => (
+              <Button
+                  key={partNumber}
+                  variant="light"
+                  color={selectedPart === partNumber ? "secondary" : "default"}
+                  onPress={() => setSelectedPart(partNumber)}
+                  className="ml-3"
+              >
+                파트 {partNumber}
+              </Button>
+          ))}
+        </div>
 
-      {renderPart(selectedPart)}
-    </div>
+        {renderPart(selectedPart)}
+      </div>
   );
 }
